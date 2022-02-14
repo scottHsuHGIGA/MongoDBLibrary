afe97e04ece78a80b07dc65cc1461cea67fac0cb
jwsdushscca <scott.hsu@hgiga.com>
2022-02-14 17:29:34 +0800

    fitst Commit

---
+++ MongoDBLibrary/Extensions/MongoExtension.cs
@@ -0,0 +1,124 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBLibrary.Extensions
+{
+    public static class MongoExtension
+    {
+        public static void Add<T>(this IMongoCollection<T> collection, T data)
+        {
+            if (data == null)
+            {
+                return;
+            }
+            collection.InsertOne(data);
+        }
+        public static void AddRange<T>(this IMongoCollection<T> collection, IEnumerable<T> datas)
+        {
+            if (datas == null || !datas.Any())
+            {
+                return;
+            }
+            collection.InsertMany(datas);
+        }
+        public static void Update<T>(this IMongoCollection<T> collection, T data)
+        {
+            var filter = GetFilterDefinition(data);
+            if (filter == null)
+            {
+                return;
+            }
+            collection.ReplaceOne(filter, data);
+        }
+        public static void UpdateRange<T>(this IMongoCollection<T> collection, IEnumerable<T> datas)
+        {
+            if (datas == null || !datas.Any())
+            {
+                return;
+            }
+            foreach (var item in datas)
+            {
+                collection.Update(item);
+            }
+        }
+        public static void Remove<T>(this IMongoCollection<T> collection, T data)
+        {
+
+            var filter = GetFilterDefinition(data);
+            if (filter == null)
+            {
+                return;
+            }
+            collection.DeleteOne(filter);
+        }
+        public static void RemoveRange<T>(this IMongoCollection<T> collection, IEnumerable<T> datas)
+        {
+            if (datas == null || !datas.Any())
+            {
+                return;
+            }
+            FilterDefinition<T> filters = GetFilterDefinition(datas);
+            if (filters == null)
+            {
+                return;
+            }
+            collection.DeleteMany(filters);
+        }
+        /// <summary>
+        /// 依 模型 取得 BsonId 的 FilterDefinition
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        private static FilterDefinition<T> GetFilterDefinition<T>(T data)
+        {
+            if (data == null)
+            {
+                return null;
+            }
+            foreach (var item in typeof(T).GetProperties())
+            {
+                if (item?.CustomAttributes.FirstOrDefault()?.AttributeType?.Name == "BsonIdAttribute")
+                {
+                    return Builders<T>.Filter.Eq(item.Name, item.GetValue(data));
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// 依 模型 將各 BsonId 的 FilterDefinition 用or 串聯起來
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="datas"></param>
+        /// <returns></returns>
+        private static FilterDefinition<T> GetFilterDefinition<T>(IEnumerable<T> datas)
+        {
+            if (datas == null || !datas.Any())
+            {
+                return null;
+            }
+            FilterDefinition<T> filters = null;
+            foreach (var item in datas)
+            {
+                var filter = GetFilterDefinition(item);
+                if (filter == null)
+                {
+                    continue;
+                }
+                if (filters == null)
+                {
+                    filters = filter;
+                }
+                else
+                {
+                    filters = filters | filter;
+                }
+            }
+            return filters;
+        }
+    }
+}
---
+++ MongoDBLibrary/Services/MongoDBService.cs
@@ -0,0 +1,74 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBLibrary.Services
+{
+    public interface IMongoDBService
+    {
+        /// <summary>
+        /// 取得 MongoDB
+        /// </summary>
+        /// <param name="dbName">資料庫 名稱</param>
+        void GetDatabase(string dbName);
+        /// <summary>
+        /// 取得 IMongoCollection
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="tableName">資料表 名稱</param>
+        /// <returns></returns>
+        IMongoCollection<T> GetCollection<T>(string tableName);
+        /// <summary>
+        /// 取得 Queryable
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="tableName">資料表 名稱</param>
+        /// <returns></returns>
+        IQueryable<T> GetQueryable<T>(string tableName);
+    }
+    public class MongoDBService : IMongoDBService
+    {
+        private static MongoClient _client;
+        private static IMongoDatabase DB;
+        public MongoDBService(string connectionString, string dbName)
+        {
+            _client = new MongoClient(connectionString);
+            GetDatabase(dbName);
+        }
+
+        /// <summary>
+        /// 取得 MongoDB
+        /// </summary>
+        /// <param name="dbName">資料庫 名稱</param>
+        public void GetDatabase(string dbName)
+        {
+            DB = _client.GetDatabase(dbName);
+        }
+
+        /// <summary>
+        /// 取得 IMongoCollection
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="tableName">資料表 名稱</param>
+        /// <returns></returns>
+        public IMongoCollection<T> GetCollection<T>(string tableName)
+        {
+            return DB.GetCollection<T>(tableName);
+        }
+
+        /// <summary>
+        /// 取得 Queryable
+        /// </summary>
+        /// <typeparam name="T">泛型</typeparam>
+        /// <param name="tableName">資料表 名稱</param>
+        /// <returns></returns>
+        public IQueryable<T> GetQueryable<T>(string tableName)
+        {
+            return GetCollection<T>(tableName).AsQueryable();
+        }
+
+    }
+}
